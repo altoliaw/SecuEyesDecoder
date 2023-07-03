8365200c3bc3b5b5b0f2ab0ee45ca854bb87bcd2
altoliaw <altoliaw@hotmail.com>
2023-07-03 17:31:17 +0800

    Mark the main file

--- Main.c
+++ Main.c
@@ -24,23 +24,22 @@ static const unsigned char base64Alphabets[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdef
  */
 int main(int argc, char** argv)
 {
-
-    unsigned char data[] = "Hello, World!";
-    unsigned int dataLength = (unsigned int)strlen((const char*)data);
-    /** Base64 encode */
-    unsigned char* encodedData = NULL;
-    encodedData = base64Encode(data, dataLength);
-    if (encodedData != NULL) {
-        printf("Encoded: %s\n", encodedData);
-        /** Base64 decode*/
-        unsigned int encodedLength = (unsigned int)strlen((char*)encodedData);
-        unsigned char* decodedData = base64Decode(encodedData, encodedLength);
-        if (decodedData != NULL) {
-            printf("Decoded: %s\n", decodedData);
-            free(decodedData);
-        }
-        free(encodedData);
-    }
+    //unsigned char data[] = "Hello, World!";
+    //unsigned int dataLength = (unsigned int)strlen((const char*)data);
+    ///** Base64 encode */
+    //unsigned char* encodedData = NULL;
+    //encodedData = base64Encode(data, dataLength);
+    //if (encodedData != NULL) {
+    //    printf("Encoded: %s\n", encodedData);
+    //    /** Base64 decode*/
+    //    unsigned int encodedLength = (unsigned int)strlen((char*)encodedData);
+    //    unsigned char* decodedData = base64Decode(encodedData, encodedLength);
+    //    if (decodedData != NULL) {
+    //        printf("Decoded: %s\n", decodedData);
+    //        free(decodedData);
+    //    }
+    //    free(encodedData);
+    //}
 
     return 0;
 }
