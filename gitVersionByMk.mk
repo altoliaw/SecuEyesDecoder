168fe6ffd07d91b3f22dc005ec91ba8727189744
altoliaw <altoliaw@hotmail.com>
2024-06-20 15:32:31 +0800

    Modifying the version comments

--- gitVersionByMk.mk
+++ gitVersionByMk.mk
@@ -1,27 +1,15 @@
 GIT_INSTALLED:=$(shell command -v git 2> /dev/null)
 GIT_HASH="None"
-Copy_RIGHT_DATE=$(shell date +"%Y")
+COPY_RIGHT_DATE=$(shell date +"%Y")
 
 ifdef GIT_INSTALLED
     # Obtaining git hash value
     GIT_HASH=$(shell git rev-parse --short HEAD)
 endif
 
-# define HEREDOC
-# SECTIONS
-# {
-#     .version :
-#     {
-#         KEEP(*(.version))
-#     }
-# }
-# endef
-
-# export HEREDOC
-
 .Phony: all
 all:
-	@$(shell echo "Version information: ${GIT_HASH} | Copyright (c) $({Copy_RIGHT_DATE}) CobraSonic/Dataisec Software" \
+	@$(shell echo "Version information: ${GIT_HASH} | Copyright (c) $({COPY_RIGHT_DATE}) CobraSonic/Dataisec Software" \
 	> git_version.txt)
 	ld -r -b binary -o git_version.o git_version.txt
 
@@ -29,6 +17,3 @@ all:
 .Phony: clean
 clean:
 	@rm -rf git_version.*
-
-# version.ld:
-# 	@echo "$$HEREDOC" > version.ld
